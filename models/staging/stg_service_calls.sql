9b806ee389182d4b3b178ac6ab98d52307855f37
victor kuang <vykuang92@gmail.com>
2023-04-24 15:38:02 +0000

    combine seasons

--- models/staging/stg_service_calls.sql
+++ models/staging/stg_service_calls.sql
@@ -14,13 +14,26 @@ select
         dbt_utils.generate_surrogate_key(
             ["creation_datetime", "ward_id", "Service_Request_Type"]
         )
-    }} as request_id, 
+    }} as request_id,
+    case
+        when
+            extract(month from creation_datetime) < 3
+            or extract(month from creation_datetime) > 11
+        then 'winter'
+        when extract(month from creation_datetime) < 6
+        then 'spring'
+        when extract(month from creation_datetime) < 9
+        then 'summer'
+        else 'fall'
+    end season,
     *
-from {{ source("staging", "facts_2022_partitioned") }}
+from
+    {{ source("staging", "facts_2022_partitioned") }}
+
+    -- dbt build --m <model.sql> --var 'is_test_run: false'
+    {% if var("is_test_run", default=true) %} tablesample system(10 percent) {% endif %}
+
 where
     ward_id is not null
     and creation_datetime is not null
     and service_request_type is not null
-    
--- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var("is_test_run", default=true) %} limit 1000 {% endif %}
